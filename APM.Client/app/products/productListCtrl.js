538e56ecd41c4dc54eabdfa8cf1fb1fd6a9c32a1
ben.micah.peterson <ben.micah.peterson@gmail.com>
2017-06-14 12:11:35 -0400

    Hard Coded Query Strings

--- APM.Client/app/products/productListCtrl.js
+++ APM.Client/app/products/productListCtrl.js
@@ -9,8 +9,10 @@
     function ProductListCtrl(productResource) {
         var vm = this;
 
+        vm.searchCriteria = "GDN";
 
-        productResource.query(function (data) {
+
+        productResource.query({search: vm.searchCriteria},function (data) {
             vm.products = data;
         })
     }
